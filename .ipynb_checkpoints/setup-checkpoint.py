45c74e042cdf16e9839b03d06520d0a74b056079
MC1316663 <70663434+MC1316663@users.noreply.github.com>
2022-12-28 17:58:37 -0700

    Initialization_1
    files

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    version = 0.1,
+    description = "algorithm for sequential calibration with variance based SA",
+    url = 'https://github.com/MC1316663/seqCalibrationWithSA',
+    author = 'Moongi Choi',
+    author_email = 'u1316663@utah.edu',
+    packages = ['sequntial_calibrationWithSA'],
+    install_packages = []
+)

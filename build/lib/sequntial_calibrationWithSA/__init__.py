8c987a72049421e52a49773f9fd649a8bf4cd74f
MC1316663 <70663434+MC1316663@users.noreply.github.com>
2022-12-30 11:58:21 -0700

    setup_4

--- build/lib/sequntial_calibrationWithSA/__init__.py
+++ build/lib/sequntial_calibrationWithSA/__init__.py
@@ -1,1 +1,3 @@
 from ex import *
+
+print("hello!")
--- setup.py
+++ setup.py
@@ -2,20 +2,25 @@ from setuptools import setup
 
 setup(
     name = 'sequntial_calibrationWithSA',
-    version = '0.0.1',
+    version = '0.0.2',
     description = "algorithm for sequential calibration with variance based SA",
     url = 'https://github.com/MC1316663/seqCalibrationWithSA.git',
     author = 'Moongi Choi',
     author_email = 'u1316663@utah.edu',
     packages = ['sequntial_calibrationWithSA'],
-    install_packages = []
+    install_requires = ['numpy']
 )
 
 
 '''note: How to make library
-- cmd -> cd repository
+- cmd -> cd repository (C:\Users\MoongiChoi\Desktop\MG\양식, 코드 등\Python\Sequential_Calibration_with_SA\seqCalibrationWithSA)
 - python setup.py bdist_wheel
 - cd dist
 - twine upload xxx.whl
+- 업데이트시에는 setup.py -> 0.02로 하고 다시 위 과정 반복
+- https://pypi.org/project/sequntial-calibrationWithSA/ 에서 확인
+
+참고:https://lsjsj92.tistory.com/592
+https://developer-theo.tistory.com/entry/%ED%8C%8C%EC%9D%B4%EC%8D%AC-GitHub-Repository-%EC%83%9D%EC%84%B1%EB%B6%80%ED%84%B0-PyPI-Package-%EB%B0%B0%ED%8F%AC%EA%B9%8C%EC%A7%80
 
 '''

ead9b4e48304ef3ea4a4f1c55b0802b712908f30
MC1316663 <70663434+MC1316663@users.noreply.github.com>
2022-12-30 11:19:58 -0700

    setup_2

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
 setup(
-    version = 0.1,
+    name = 'seqCalibrationWithSA',
+    version = 0.0.1,
     description = "algorithm for sequential calibration with variance based SA",
     url = 'https://github.com/MC1316663/seqCalibrationWithSA.git',
     author = 'Moongi Choi',
     author_email = 'u1316663@utah.edu',
-    packages = ['sequntial_calibrationWithSA'],
+    packages = ['seqCalibrationWithSA'],
     install_packages = []
 )
+
+
+'''note: How to make library
+- cmd -> cd repository
+- python setup.py bdist_wheel
+- cd dist
+- twine upload xxx.whl
+
+'''
